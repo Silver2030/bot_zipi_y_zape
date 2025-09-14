25350da82ca361daa571e482738292b039d62cb8
Iván <ivanrq88@educastur.es>
2025-09-14 13:48:20 +0200

    Variable token agregada

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-const token = 'TOKEN';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 // Lista de usuarios
