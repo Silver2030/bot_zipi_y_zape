60c11cf3c1b9a7d7e94591560338af9b8230d718
Ivan <ivanrqovin@gmail.com>
2025-11-21 14:53:15 +0100

    Metodo faltante agregado

--- bot.js
+++ bot.js
@@ -332,6 +332,22 @@ async function procesarGrupoDanyo(chatId, args, tipo) {
     }
 }
 
+function analizarBuild(userData) {
+    let pvpPoints = 0, ecoPoints = 0;
+    
+    PVP_SKILLS.forEach(skill => pvpPoints += SKILL_COSTS[userData.skills[skill]?.level || 0]);
+    ECO_SKILLS.forEach(skill => ecoPoints += SKILL_COSTS[userData.skills[skill]?.level || 0]);
+
+    const total = pvpPoints + ecoPoints;
+    const pctPvp = total ? (pvpPoints / total) * 100 : 0;
+
+    let build = "HIBRIDA";
+    if (pctPvp > 65) build = "PVP";
+    else if (pctPvp < 35) build = "ECO";
+
+    return { build, nivel: userData.leveling?.level || 0 };
+}
+
 function obtenerEstadoPastilla(userData) {
     const buffs = userData.buffs;
     if (buffs?.buffCodes?.length) {
