7774c6660185f9f1fc707e5bb01558c6882bf234
Ivan <ivanrqovin@gmail.com>
2025-12-18 20:00:16 +0100

    Corrección de post a get en apis

--- services/apiService.js
+++ services/apiService.js
@@ -1,36 +1,40 @@
 const axios = require('axios');
 const { delay } = require('../utils/helpers');
 
-let lastApiCall = 0;
-const MIN_API_INTERVAL = 200; // ms
+let lastCall = 0;
+const MIN_DELAY = 500;
 
 async function apiCall(method, params = {}) {
     const now = Date.now();
-    const wait = Math.max(0, MIN_API_INTERVAL - (now - lastApiCall));
-    if (wait > 0) await delay(wait);
-
-    lastApiCall = Date.now();
+    const timeSinceLastCall = now - lastCall;
+    if (timeSinceLastCall < MIN_DELAY) {
+        await delay(MIN_DELAY - timeSinceLastCall);
+    }
 
     try {
-        const res = await axios.post(
-            `https://api2.warera.io/trpc/${method}`,
-            params
-        );
+        const res = await axios.get(`https://api2.warera.io/trpc/${method}`, {
+            params: { input: JSON.stringify(params) }
+        });
+        lastCall = Date.now();
         return res.data?.result?.data ?? null;
     } catch (error) {
         console.error(`Error en API ${method}:`, error.message);
+        lastCall = Date.now();
         return null;
     }
 }
 
-const getUserData = (userId) =>
-    apiCall('user.getById', { userId });
+async function getUserData(userId) {
+    return apiCall('user.getById', { userId });
+}
 
-const getCountryData = (countryId) =>
-    apiCall('country.getById', { countryId });
+async function getCountryData(countryId) {
+    return apiCall('country.getById', { countryId });
+}
 
-const getMUData = (muId) =>
-    apiCall('mu.getById', { muId });
+async function getMUData(muId) {
+    return apiCall('mu.getById', { muId });
+}
 
 module.exports = {
     apiCall,
