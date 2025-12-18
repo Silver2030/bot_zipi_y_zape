22ceb550bf88704d21c7bed5c795ad146066e448
Ivan <ivanrqovin@gmail.com>
2025-12-18 20:33:37 +0100

    Cambio en el escapeMrkdownV2

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,8 @@
 function escapeMarkdownV2(text) {
-    return text.replace(/([_*\[\]()~`>#+\-=|{}.!])/g, '\\$1');
+    if (typeof text !== 'string') {
+        text = String(text);
+    }
+    return text.replace(/([_*[\]()~`>#+\-=|{}.!])/g, '\\$1');
 }
 
 function formatNumber(num) {
