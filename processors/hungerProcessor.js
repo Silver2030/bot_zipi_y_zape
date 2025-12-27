5ac5e15d3fd55c8029bd878f1e50456e6cece12b
Ivan <ivanrqovin@gmail.com>
2025-12-27 20:39:01 +0100

    Fix en el ping de hunger

--- processors/hungerProcessor.js
+++ processors/hungerProcessor.js
@@ -3,9 +3,11 @@ const { analizarBuild, escapeMarkdownV2 } = require('../utils/helpers');
 
 async function hambre(botQueue, chatId, args, usuarios) {
   if (!args[0]) {
-    return botQueue.sendMessage(chatId, "Ejemplo: /hambre https://app.warera.io/battle/68c5efa7d9737c88a4da826c DEFENDEMOS CON TODO", {
-      disable_web_page_preview: true
-    });
+    return botQueue.sendMessage(
+      chatId,
+      "Ejemplo: /hambre https://app.warera.io/battle/68c5efa7d9737c88a4da826c DEFENDEMOS CON TODO",
+      { disable_web_page_preview: true }
+    );
   }
 
   let urlBattle = args[0];
@@ -26,8 +28,7 @@ async function hambre(botQueue, chatId, args, usuarios) {
 
       const hunger = userData.skills?.hunger;
       if (hunger && hunger.currentBarValue >= 0.3 * hunger.total) {
-        const mention = `[${escapeMarkdownV2(userData.username)}](${urlBattle})`;
-        menciones.push(mention);
+        menciones.push(usuario.mention);
       }
     } catch (error) {
       console.error(`Error con usuario ${usuario.userId}:`, error.message);
@@ -38,8 +39,10 @@ async function hambre(botQueue, chatId, args, usuarios) {
     return botQueue.sendMessage(chatId, "Nadie tiene comida");
   }
 
-  await botQueue.sendMessage(chatId, 
-    `${escapeMarkdownV2(urlBattle)}\n${escapeMarkdownV2(mensajeExtra)}`, {
+  await botQueue.sendMessage(
+    chatId,
+    `${escapeMarkdownV2(urlBattle)}\n${escapeMarkdownV2(mensajeExtra)}`,
+    {
       disable_web_page_preview: true,
       parse_mode: "MarkdownV2"
     }
@@ -49,7 +52,6 @@ async function hambre(botQueue, chatId, args, usuarios) {
   for (let i = 0; i < menciones.length; i += chunkSize) {
     const grupo = menciones.slice(i, i + chunkSize).join('\n');
     await botQueue.sendMessage(chatId, grupo, {
-      parse_mode: "MarkdownV2",
       disable_web_page_preview: true
     });
   }
