c8efa0fff5bc9b21f9090f4be3d59734b831bade
Ivan <ivanrqovin@gmail.com>
2026-04-04 19:22:43 +0200

    Fixeo en los nombres de los rankings

--- commands/ranking.js
+++ commands/ranking.js
@@ -2,7 +2,8 @@
 
 const tg = require("../telegram");
 const { t } = require("../i18n");
-const { apiCall } = require("../api");
+const { apiCall, getAllCountries } = require("../api");
+const { fetchUsersLite } = require("../fetchers");
 const { escapeMarkdownV2, formatNumberMarkdown } = require("../utils");
 
 const TIPOS_VALIDOS = {
@@ -34,13 +35,25 @@ async function ranking(chatId, args) {
 
     if (!items.length) { await tg.sendMessage(chatId, t(chatId, "no_results")); return; }
 
-    console.log("[ranking] primer item:", JSON.stringify(items[0], null, 2));
-
     const top10 = items.slice(0, 10);
+    const esCountry = config.key === "weeklyCountryDamages";
+
+    // Resolver nombres
+    let nombreMap = new Map();
+    if (esCountry) {
+      const countries = await getAllCountries();
+      countries.forEach((c) => nombreMap.set(c._id, c.name));
+    } else {
+      const userIds  = top10.map((x) => x.user).filter(Boolean);
+      const usersData = await fetchUsersLite(userIds, { batchSize: 30 });
+      userIds.forEach((id, idx) => { if (usersData[idx]) nombreMap.set(id, usersData[idx].username); });
+    }
+
     let msg = `*${escapeMarkdownV2(config.label)}*\n\n`;
     top10.forEach((item, i) => {
-      const nombre = escapeMarkdownV2(item.username ?? item.name ?? item.countryName ?? "?");
-      const valor  = formatNumberMarkdown(item.value ?? item.damage ?? 0);
+      const id     = esCountry ? item.country : item.user;
+      const nombre = escapeMarkdownV2(nombreMap.get(id) ?? "?");
+      const valor  = formatNumberMarkdown(item.value ?? 0);
       msg += `${i + 1}\\. ${nombre}: ${valor}\n`;
     });
 
